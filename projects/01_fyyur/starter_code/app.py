9b180befe9f37aa5cc6c152f60aa146b8d771f52
Huijun Wang <suewang91@gmail.com>
2022-06-07 21:41:50 -0400

    complete project

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -26,17 +26,67 @@ from forms import *
 #----------------------------------------------------------------------------#
 # Import models.
 #----------------------------------------------------------------------------#
-from models import Venue, Artist, Show
 
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+moment = Moment(app)
+
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+        __tablename__ = 'venues'
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String)
+        city = db.Column(db.String(120))
+        state = db.Column(db.String(120))
+        address = db.Column(db.String(120))
+        phone = db.Column(db.String(120))
+        genres = db.Column(db.ARRAY(db.String), nullable=False)
+        website = db.Column(db.String(120))
+        image_link = db.Column(db.String(500))
+        facebook_link = db.Column(db.String(120))
+        seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+        seeking_description = db.Column(db.String(2000))
+        shows = db.relationship('Show', backref = "venue", lazy=True, 
+                cascade="all, delete")
+
+class Artist(db.Model):
+        __tablename__ = 'artists'
+
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String)
+        city = db.Column(db.String(120))
+        state = db.Column(db.String(120))
+        phone = db.Column(db.String(120))
+        genres = db.Column(db.ARRAY(db.String), nullable=False)
+        website = db.Column(db.String(120))
+        image_link = db.Column(db.String(500))
+        facebook_link = db.Column(db.String(120))
+        seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+        seeking_description = db.Column(db.String(2000))
+        shows = db.relationship('Show', backref = "artist", lazy=True, 
+                cascade="all, delete")
+
+class Show(db.Model):
+        __tablename__ = "shows"
+        id = db.Column(db.Integer, primary_key=True)
+        start_time = db.Column(db.DateTime())
+        venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+        artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# app = Flask(__name__)
+# moment = Moment(app)
+# app.config.from_object('config')
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
 
 #----------------------------------------------------------------------------#
@@ -114,7 +164,7 @@ def search_venues():
 def show_venue(venue_id):
     
     # basic information about the venue
-    venue = Venue.query.filter_by(id=venue_id).all()[0]
+    venue = Venue.query.filter_by(id=venue_id).first()
     data = {}
     data['id'] = venue.id
     data['name'] = venue.name
@@ -194,21 +244,20 @@ def create_venue_submission():
         db.session.close()
     return render_template('pages/home.html')
 
-@app.route('/venues/<venue_id>', methods=['DELETE'])
+@app.route('/venues/<int:venue_id>/delete', methods=['POST'])
 def delete_venue(venue_id):
     try:
-        venue = Venue.query.get(venue_id)
-        venue_name = venue.name
+        venue = Venue.query.filter_by(id=venue_id).first()
         db.session.delete(venue)
         db.session.commit()
-        flash(f'Venue {venue_id} {venue_name} was successfully deleted!')
-    except:
+        flash('The venue has been removed together with all of its shows.')
+        return render_template('pages/home.html')
+    except ValueError:
         db.session.rollback()
-        flash(f'An error occurred. Venue {venue_id} could not be deleted.')
+        flash('It was not possible to delete this Venue')
     finally:
-        db.session.close() 
-
-    return render_template('pages/home.html')
+        db.session.close()
+    return redirect(url_for('venues'))
 
 #    Artists
 #    ----------------------------------------------------------------
